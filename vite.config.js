c48666ee609f884f8f92702008695d325315c82b
zamirul <javshazib@gmail.com>
2025-12-17 23:44:54 +0600

    camera geomatrice add done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import topLevelAwait from 'vite-plugin-top-level-await'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import topLevelAwait from "vite-plugin-top-level-await";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [topLevelAwait()],
-  base: './',
-})
+  plugins: [react(), topLevelAwait(), tailwindcss()],
+  base: "./",
+});
